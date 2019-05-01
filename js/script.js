f0c173814659f580fa7d4a96fe45fb8f44278c2b
Abdullah Mahmood <Abdullahmhm2018@gmail.com>
2019-05-01 12:31:27 +0800

    Added Some Car Stuff

--- js/script.js
+++ js/script.js
@@ -1,4 +1,28 @@
-	console.log('welcome');
-	console.log('to my yt chanel');
-	var myName = 'Abdullah';
-	console.log (myName);
+var sayMyName = function (name) {
+	alert ('My Name Is: ' +name)
+} 
+
+var car ={
+	color: 'green',
+	isCarTurnedOn: false,
+	numberOfWheels: 4,
+	seats: [
+	'seat 1',
+	'seat 2',
+	'seat 3',
+	'seat 4'
+	],
+	turnOn: function(){
+	this.isCarTurnedOn = true;
+	},
+	fly: function (){
+		alert('ur a nigger');
+	},
+	switchCar: function (isOn){
+		if (isOn == true) {
+			this.isCarTurnedOn = true;
+		} else {
+			this.isCarTurnedOn = false;
+		}
+	}
+}
